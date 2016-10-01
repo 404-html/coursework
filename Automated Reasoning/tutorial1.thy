63029898219c116b6b2bb570a7dec2deac987462
Jianmeng Yu <s1413557@sms.ed.ac.uk>
2016-10-01 18:35:03 +0100

    Add files via upload

---
+++ Automated Reasoning/tutorial1.thy
@@ -0,0 +1,72 @@
+theory tutorial1
+imports Main
+
+begin
+
+theorem "A \<longrightarrow> B \<longrightarrow> A"
+apply (rule impI)
+apply (rule impI)
+apply assumption
+done
+
+lemma exercise4: "(R\<longrightarrow>P)\<longrightarrow>(((\<not>R\<or>P)\<longrightarrow>(Q\<longrightarrow>S))\<longrightarrow>(Q\<longrightarrow>S))"
+apply (rule impI)
+apply (rule impI)
+apply (rule impI)
+apply (erule impCE)
+apply (erule impE)
+apply (erule disjI1)
+apply (erule mp)
+apply assumption
+apply (erule impE)
+apply (erule disjI2)
+apply (erule mp)
+apply assumption
+done
+
+lemma exercise51: "( P \<longrightarrow>( Q \<longrightarrow> R )) \<longrightarrow>  (( P \<longrightarrow> Q )\<longrightarrow>( P\<longrightarrow> R))"
+apply (rule impI)
+apply (rule impI)
+apply (rule impI)
+apply (drule mp)
+apply assumption
+apply (erule mp)
+apply (erule mp)
+apply assumption
+done
+
+lemma exercise52: "\<not>\<not>P \<longrightarrow> P"
+apply (rule impI)
+apply (erule notnotD)
+done
+
+lemma exercise53: "(P \<longrightarrow> Q \<and> R) \<longrightarrow> ((P \<longrightarrow> Q) \<and> (P \<longrightarrow> R))"
+apply (rule impI)
+apply (rule context_conjI)
+apply (rule impI)
+apply (drule mp)
+apply assumption
+apply (drule conjunct1)
+apply assumption
+apply (rule impI)
+apply (drule mp)
+apply assumption
+apply (drule conjunct2)
+apply assumption
+done
+
+lemma exercise54: "(\<not>P \<longrightarrow> Q)\<longrightarrow>(\<not>Q \<longrightarrow> P)"
+apply (rule impI)
+apply (rule impI)
+apply (rule classical)
+apply (erule notE)
+apply (erule mp)
+apply assumption
+done
+
+lemma exercise55: "P \<or> \<not> P"
+apply (rule disjCI)
+apply (erule notnotD)
+done
+
+end
